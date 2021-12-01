ebd1a35e73bceda41f33f8bcd3d1876e83d05c0e
46kuri_ <ghost.46kuri@gmail.com>
2021-12-01 14:46:32 +0900

    package名変更

---
+++ src/main/java/com/github/sirokuri_/remakeskull/Remakeskull.java
@@ -0,0 +1,20 @@
+package com.github.sirokuri_.remakeskull;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Remakeskull extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        getCommand("skullgive").setExecutor(new Remakeskullcommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
